d845d47c3d29c4f84eb9988dea87c03517826fe7
Guilherme Torres <jguilherme.torres@gmail.com>
2020-07-02 00:48:41 -0300

    Refatoração da tela de produtos

--- lib/app/src/views/products_views.dart
+++ lib/app/src/views/products_views.dart
@@ -1,6 +1,4 @@
-import 'package:agencia_scholz/app/src/components/custom_drawer_components.dart';
 import 'package:agencia_scholz/app/src/components/product_tile_components.dart';
-import 'package:agencia_scholz/app/src/data/product_data.dart';
 import 'package:agencia_scholz/app/src/models/product_manager_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
