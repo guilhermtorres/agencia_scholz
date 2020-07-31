6e94e3f6f7030044e48ef17ea1ed079a3df07418
Guilherme Torres <jguilherme.torres@gmail.com>
2020-07-31 03:54:05 -0300

    Salvando tela inicial no Firebase

--- lib/app/src/data/section_data.dart
+++ lib/app/src/data/section_data.dart
@@ -1,20 +1,33 @@
+import 'dart:io';
+
 import 'package:agencia_scholz/app/src/data/section_item_data.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/widgets.dart';
+import 'package:uuid/uuid.dart';
 
 class Section extends ChangeNotifier {
-  Section({this.name, this.type, this.items}) {
+  Section({this.id, this.name, this.type, this.items}) {
     items = items ?? [];
+    originalItems = List.from(items);
   }
   Section.fromDocument(DocumentSnapshot document) {
+    id = document.documentID;
     name = document.data['name'] as String;
     type = document.data['type'] as String;
     items = (document.data['items'] as List).map((i) => SectionItem.fromMap(i as Map<String, dynamic>)).toList();
   }
 
+  final Firestore firestore = Firestore.instance;
+  final FirebaseStorage storage = FirebaseStorage.instance;
+
+  DocumentReference get firestoreRef => firestore.document('home/$id');
+  StorageReference get storageRef => storage.ref().child('home').child(id);
+  String id;
   String name;
   String type;
   List<SectionItem> items;
+  List<SectionItem> originalItems;
 
   String _error;
   String get error => _error;
@@ -33,6 +46,46 @@ class Section extends ChangeNotifier {
     notifyListeners();
   }
 
+  Future<void> save() async {
+    final Map<String, dynamic> data = {
+      'name': name,
+      'type': type,
+    };
+
+    if (id == null) {
+      final doc = await firestore.collection('home').add(data);
+      id = doc.documentID;
+    } else {
+      await firestoreRef.updateData(data);
+    }
+
+    for (final item in items) {
+      if (item.image is File) {
+        final StorageUploadTask task = storageRef.child(Uuid().v1()).putFile(item.image as File);
+        final StorageTaskSnapshot snapshot = await task.onComplete;
+        final String url = await snapshot.ref.getDownloadURL() as String;
+        item.image = url;
+      }
+    }
+
+    for (final original in originalItems) {
+      if (!items.contains(original)) {
+        try {
+          final ref = await storage.getReferenceFromUrl(original.image as String);
+
+          await ref.delete();
+          // ignore: empty_catches
+        } catch (e) {}
+      }
+    }
+
+    final Map<String, dynamic> itemsData = {
+      'items': items.map((e) => e.toMap()).toList(),
+    };
+
+    await firestoreRef.updateData(itemsData);
+  }
+
   bool valid() {
     if (name == null || name.isEmpty) {
       error = 'Título inválido';
@@ -46,6 +99,7 @@ class Section extends ChangeNotifier {
 
   Section clone() {
     return Section(
+      id: id,
       name: name,
       type: type,
       items: items.map((e) => e.clone()).toList(),
